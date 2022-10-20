b11c4765aa94f53f4e5bd93103f234e30cc2dcb2
«Denis» <denvoropaev7@gmail.com>
2022-10-20 22:56:32 +0300

    Запись в файл csv

--- write.py
+++ write.py
@@ -1,5 +1,10 @@
-def write_csv():
-    pass
+import csv
+
+def write_csv(empl_data):
+    with open('database.csv', 'w', encoding="utf-8") as file:
+        writer = csv.writer(file)
+        for empl in empl_data:
+            writer.writerow(empl.values())
 
 def write_json():
     pass
